48ea0575bf91af1b1adc622588ea13d223dd0606
Rodolfo Pazos Rivas <rodopazos@gmail.com>
2021-02-19 13:23:35 -0300

    add command variable

--- www/js/index.js
+++ www/js/index.js
@@ -16,13 +16,22 @@ class App {
 // ------------------------------------------------
 //                      Main
 // ------------------------------------------------
-let app = new App();
+const app = new App();
+
+const command = `
+    ^XA
+    ^FO50,50
+    ^B8N,100,Y,N
+    ^FD1234567
+    ^FS
+    ^XZ
+`;
 
 app.zebra.searchForPrinters()
     .then(printers => alert("searchForPrinters-done" + JSON.stringify(printers)))
     .catch(error => alert("searchForPrinters-error" + JSON.stringify(error)));
 
-app.zebra.broadcastCommand()
+app.zebra.broadcastCommand(command)
     .then(result => alert("broadcastCommand-done" + JSON.stringify(result)))
     .catch(error => alert("broadcastCommand-error" + JSON.stringify(error)));
 
