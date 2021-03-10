959e79550cb7bea9c8049c011abeb234e4e2d656
Rodolfo Pazos Rivas <rodopazos@gmail.com>
2021-03-10 09:50:55 -0300

    test other plugin

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,7 @@
 class App {
     constructor() {
         this._zebra = new Zebra();
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('deviceready', this.onDeviceReady1.bind(this), false);
     }
 
     get zebra() {
@@ -45,44 +45,9 @@ class App {
     onDeviceReady1() {
         document.getElementById('deviceready').classList.add('ready');
 
-        const txtCmdScheme = {
-            xOrigin: 50,
-            yOrigin: 50,
-            font: 2,
-            fontHeight: 50,
-            fontWidth: 50,
-            data: 'Jose Marti'
-        };
-        const barCmdScheme = {
-            xOrigin: 50,
-            yOrigin: 120,
-            orientation: 'I',
-            barHeight: 50,
-            data: '28-01-1853'
-        };
-        const boxCmdScheme = {
-            xOrigin: 50,
-            yOrigin: 190,
-            boxWidth: 200,
-            boxHeight: 200
-        };
-        const txtCmd = `^XA${this.zebra.buildTextCmd(txtCmdScheme)}^XZ`;
-        const barCmd = `^XA${this.zebra.buildBarcodeCmd(barCmdScheme)}^XZ`;
-        const boxCmd = `^XA${this.zebra.buildBoxCmd(boxCmdScheme)}^XZ`;
-        const allCmd = `
-            ^XA
-                ${this.zebra.buildBarcodeCmd(txtCmdScheme)}
-                ${this.zebra.buildBarcodeCmd(barCmdScheme)}
-                ${this.zebra.buildBoxCmd(boxCmdScheme)}
-            ^XZ`;
-
         this.zebra.searchForPrinters()
             .then(printers => {
                 alert("searchForPrinters-done" + JSON.stringify(printers));
-
-                this.zebra.broadcastCommand(allCmd)
-                    .then(result => alert("broadcastCommand-done" + JSON.stringify(result)))
-                    .catch(error => alert("broadcastCommand-error" + JSON.stringify(error)));
             })
             .catch(error => alert("searchForPrinters-error" + JSON.stringify(error)));
     }
